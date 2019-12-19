b7b55bcf99fc4b5fb00a8ab576b88afff701ba24
Bob <byzhao@umich.edu>
2019-12-19 16:25:17 -0500

    commit 3

--- script.R
+++ script.R
@@ -1,4 +1,3 @@
-summation = 0
-for (i in 1:100)
-  summation = summation + sqrt(i)
-save(summation, file = "summation.Rdata")
+summation = SLURM_ARRAY_TASK_ID
+filename=paste0("summation",SLURM_ARRAY_TASK_ID,".Rdata")
+save(summation, file = filename)
